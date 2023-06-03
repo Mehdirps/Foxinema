5795ab06f552f18d0d522eef2077cdf979ce69a2
Mehdirps <mehdi.raposo77@gmail.com>
2023-06-03 10:29:58 +0200

    🐛 fix : dbConfig

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const OpinionRoutes = require('./routes/opinion.routes');
 const PostRoutes = require('./routes/post.routes');
 const CommentRoutes = require('./routes/comment.routes');
 require('dotenv').config({ path: '/.env' })
-require('./config/dbConfig');
+require('./dbConfig');
 const { requireAuth } = require('./middlewares/auth.middleware');
 const cors = require('cors');
 const nodemailer = require('nodemailer');
